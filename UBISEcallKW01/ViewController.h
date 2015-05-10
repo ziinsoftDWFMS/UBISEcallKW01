972e98175aae0e27f65974dd116dc12761b6cd50
youngseok Kim <youngseokkim@youngseokui-MacBook-Pro.local>
2015-05-10 21:55:33 +0900

    copy

---
+++ UBISEcallKW01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UBISEcallKW01
+//
+//  Created by youngseok Kim on 2015. 5. 10..
+//  Copyright (c) 2015년 dwni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UBISEcallKW01/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UBISEcallKW01
+//
+//  Created by youngseok Kim on 2015. 5. 10..
+//  Copyright (c) 2015년 dwni. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
